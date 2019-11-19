4738c40c3b6e2a08ee24f2fd51731a9805768626
Artem Bobrov <artem.bobrov@ddgcorp.ru>
2019-11-19 18:55:58 +0300

    added connection object to methods signatures

--- lib-interface/src/main/java/ru/ddg/ocular/lib/interfaces/services/LibraryService.java
+++ lib-interface/src/main/java/ru/ddg/ocular/lib/interfaces/services/LibraryService.java
@@ -9,73 +9,80 @@ import java.util.List;
 public interface LibraryService {
 
     /**
+     * Подключение к очереди
+     * @param address адрес сервера
+     * @param port порт
+     * @return объект соединения
+     * @throws Exception ошибка подключения
+     */
+    Connection connect(String address, int port) throws Exception;
+
+    /**
+     * Отключение от очереди
+     * @param connection объект соединения
+     * @throws Exception ошибка отключения
+     */
+    void disconnect(Connection connection) throws Exception;
+
+    /**
      * Возвращает информацию о сервере
      * @param serverName имя сервера
+     * @param connection объект соединения
      * @return информация о сервере
      * @see ServerState;
      */
-    ServerState getServerState(String serverName);
+    ServerState getServerState(Connection connection, String serverName);
 
     /**
      * Отправляет команду 'reset' на сервер ocular
      * @param serverName имя сервера
+     * @param connection объект соединения
      * @throws Exception
      */
-    void resetServer(String serverName) throws Exception;
-
-    /**
-     * Подключение к очереди
-     * @param address адрес сервера
-     * @param port порт
-     * @param serverName имя сервера
-     * @return объект соединения
-     * @throws Exception ошибка подключения
-     */
-    Connection connect(String address, int port, String serverName) throws Exception;
-
-    /**
-     * Отключение от очереди
-     * @throws Exception ошибка отключения
-     */
-    void disconnect(String serverName) throws Exception;
+    void resetServer(Connection connection, String serverName) throws Exception;
 
     /**
      * Добавляет новую камеру
      * @param camera камера
+     * @param connection объект соединения
      * @throws Exception ошибка добавления камеры
      */
-    void addCamera(Camera camera) throws Exception;
+    void addCamera(Connection connection, Camera camera) throws Exception;
 
     /**
      * Удаляет существующую камеру
      * @param cameraId идентификатор камеры
-     * @param serverName имя сервера
+     * @param connection объект соединения
      * @throws Exception ошибка удаления камеры
      */
-    void removeCamera(long cameraId, String serverName) throws Exception;
+    void removeCamera(Connection connection, long cameraId) throws Exception;
 
     /**
      * Возвращает список камер
      * @return список камер List<Camera>
+     * @param connection объект соединения
      * @param serverName имя сервера
      * @see Camera
      */
-    List<Camera> getCameraList(String serverName);
+    List<Camera> getCameraList(Connection connection, String serverName);
 
     /**
      * Переключает режим записи камеры.
      * @param camera Камера
-     * @param serverName имя сервера
+     * @param connection объект соединения
      * @param isRecording включена true или выключена false
+     * @param serverName имя сервера
      * @throws Exception ошибка переключения режима записи
      */
-    void setRecording(Camera camera, String serverName, boolean isRecording) throws Exception;
+    void setRecording(Connection connection, String serverName, Camera camera, boolean isRecording) throws Exception;
 
     /**
      * Управление ptz камерой
+     * @param connection объект соединения
+     * @param serverName имя сервера
      * @param vertical движение в вертикальном направлении
      * @param horizontal движение в горизонтальном направлении
      * @param zoom приближение
      */
-    void ptzControl(int vertical, int horizontal, int zoom);
+    void ptzControl(Connection connection, String serverName, int vertical, int horizontal, int zoom);
 }
