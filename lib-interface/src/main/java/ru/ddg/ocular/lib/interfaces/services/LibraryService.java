ca7fdf8e2e6526676cc4573f661d56fff908ccb5
Artem Bobrov <artem.bobrov@ddgcorp.ru>
2019-11-19 18:12:21 +0300

    refactored library interface

--- lib-interface/src/main/java/ru/ddg/ocular/lib/interfaces/services/LibraryService.java
+++ lib-interface/src/main/java/ru/ddg/ocular/lib/interfaces/services/LibraryService.java
@@ -10,33 +10,34 @@ public interface LibraryService {
 
     /**
      * Возвращает информацию о сервере
-     * @param address адрес сервера
-     * @param port порт
+     * @param serverName имя сервера
      * @return информация о сервере
      * @see ServerState;
      */
-    ServerState getServerState(String address, int port);
+    ServerState getServerState(String serverName);
 
     /**
      * Отправляет команду 'reset' на сервер ocular
+     * @param serverName имя сервера
      * @throws Exception
      */
-    void resetServer() throws Exception;
+    void resetServer(String serverName) throws Exception;
 
     /**
      * Подключение к очереди
      * @param address адрес сервера
      * @param port порт
+     * @param serverName имя сервера
      * @return объект соединения
      * @throws Exception ошибка подключения
      */
-    Connection connect(String address, int port) throws Exception;
+    Connection connect(String address, int port, String serverName) throws Exception;
 
     /**
      * Отключение от очереди
      * @throws Exception ошибка отключения
      */
-    void disconnect() throws Exception;
+    void disconnect(String serverName) throws Exception;
 
     /**
      * Добавляет новую камеру
@@ -48,24 +49,33 @@ public interface LibraryService {
     /**
      * Удаляет существующую камеру
      * @param cameraId идентификатор камеры
-     * @param address адрес сервера
-     * @param port порт
+     * @param serverName имя сервера
      * @throws Exception ошибка удаления камеры
      */
-    void removeCamera(long cameraId, String address, int port) throws Exception;
+    void removeCamera(long cameraId, String serverName) throws Exception;
 
     /**
      * Возвращает список камер
      * @return список камер List<Camera>
+     * @param serverName имя сервера
      * @see Camera
      */
-    List<Camera> getCameraList();
+    List<Camera> getCameraList(String serverName);
 
     /**
      * Переключает режим записи камеры.
      * @param camera Камера
+     * @param serverName имя сервера
      * @param isRecording включена true или выключена false
      * @throws Exception ошибка переключения режима записи
      */
-    void setRecording(Camera camera, boolean isRecording) throws Exception;
+    void setRecording(Camera camera, String serverName, boolean isRecording) throws Exception;
+
+    /**
+     * Управление ptz камерой
+     * @param vertical движение в вертикальном направлении
+     * @param horizontal движение в горизонтальном направлении
+     * @param zoom приближение
+     */
+    void ptzControl(int vertical, int horizontal, int zoom);
 }
