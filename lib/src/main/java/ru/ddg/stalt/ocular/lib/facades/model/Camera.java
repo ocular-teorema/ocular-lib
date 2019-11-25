53646ba96302a5d82a2b9974974c3a63078840ec
Artem Bobrov <artem.bobrov@ddgcorp.ru>
2019-11-25 18:31:14 +0300

    added fields

--- lib/src/main/java/ru/ddg/stalt/ocular/lib/facades/model/Camera.java
+++ lib/src/main/java/ru/ddg/stalt/ocular/lib/facades/model/Camera.java
@@ -5,15 +5,33 @@ import lombok.Getter;
 
 /**
  * Данный объект содержит информацию о камере:
- * имя, адрес, время хранения видео (дней), адрес архива.
+ * Идентификатор, имя, основной адрес (видеопоток с максимальным разрешением),
+ * дополнительный адрес (опционально, видеопоток с меньшим разрешением), идентификатор хранилища,
+ * идентификатор расписания, тип анализа (1 - без анализа, 2 - только движение, 3 - полный анализ), статус,
+ * статус записи (true - включена, false - выключена)
+ * время хранения видео (дней), адрес архива.
  */
 @Getter
 @AllArgsConstructor
 public class Camera {
 
+    String cameraId;
+
     String name;
 
-    String address;
+    String primaryAddress;
+
+    String secondaryAddress;
+
+    Integer storageId;
+
+    Integer scheduleId;
+
+    Integer analysisType;
+
+    String status;
+
+    Boolean isRecording;
 
     Integer storageDays;
 
