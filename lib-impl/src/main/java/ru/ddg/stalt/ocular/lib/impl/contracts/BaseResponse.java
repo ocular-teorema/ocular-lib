536b539b0b72d9055cfd2bea41fc0ef8f59b0e79
Artem Bobrov <artem.bobrov@ddgcorp.ru>
2019-12-06 19:34:01 +0300

    fix UUID

--- lib-impl/src/main/java/ru/ddg/stalt/ocular/lib/impl/contracts/BaseResponse.java
+++ lib-impl/src/main/java/ru/ddg/stalt/ocular/lib/impl/contracts/BaseResponse.java
@@ -1,9 +1,12 @@
 package ru.ddg.stalt.ocular.lib.impl.contracts;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @Getter
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
@@ -18,7 +21,8 @@ import lombok.Getter;
 })
 public class BaseResponse {
 
-    private String uuid;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private UUID requestUuid;
 
     private boolean success;
     @JsonProperty("code")
