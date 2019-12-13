03316ed315caf3429edf50fce76743d3dde32728
Artem Bobrov <artem.bobrov@ddgcorp.ru>
2019-12-13 14:49:38 +0300

    added test  scenario

--- test-app/src/main/java/ru/ddg/stalt/ocular/lib/test/Application.java
+++ test-app/src/main/java/ru/ddg/stalt/ocular/lib/test/Application.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Import;
 import ru.ddg.stalt.ocular.lib.OcularLibModule;
 import ru.ddg.stalt.ocular.lib.model.Connection;
 import ru.ddg.stalt.ocular.lib.services.OcularService;
+import ru.ddg.stalt.ocular.lib.model.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @SpringBootApplication
@@ -29,7 +33,38 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         try (Connection connection = ocularService.connect("78.1.2.3", 1234, "test", "test", 3000, "1")) {
-            ocularService.getCameraList(connection, "test");
+
+            List<Camera> cameraList = ocularService.getCameraList(connection,"448A5B2C2B54");
+            Optional<Camera> runningCamera;
+            String streamAddress;
+            String cameraId;
+
+            if (cameraList.isEmpty()) {
+                Camera camera = new Camera();
+                camera.setName("camera_test");
+                camera.setPrimaryAddress("test");
+                camera.setStatus(CameraStatusEnum.RUNNING);
+                camera.setEnabled(true);
+                camera.setStorageDays(7);
+                camera.setAnalysisType(2);
+                camera.setStorageId(1);
+                ocularService.addCamera(connection,camera,"448A5B2C2B54");
+
+                runningCamera = ocularService.getCameraList(connection,"448A5B2C2B54")
+                        .stream()
+                        .findAny();
+            }
+            else {
+                runningCamera = cameraList
+                        .stream()
+                        .filter(camera -> (camera.getStatus() == CameraStatusEnum.RUNNING))
+                        .findAny();
+            }
+            if (runningCamera.isPresent()) {
+                streamAddress = runningCamera.get().getStreamAddress();
+                cameraId = runningCamera.get().getCameraId();
+                log.info("Camera stream received: {} camera id is {}", streamAddress, cameraId);
+            }
         }
     }
 }
